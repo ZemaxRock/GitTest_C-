532b2eaa84dbfb0ffc41f447b0d3ee82888f2577
ZXC <114514@qq.com>
2022-11-07 03:27:40 +0800

    Frist

---
+++ C++Learning/C++Learning/源.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << "123";
+	//变量
+	int a = 1;
+	double b = 12.10;
+	float c = 1.3;
+	char d = 'a';
+	string e = "c++";
+	bool f = true;
+
+	cout << a << endl;
+	cout << "这是一个浮点数: " << b << "\n对吧！" << endl;
+	return 0;
+}
